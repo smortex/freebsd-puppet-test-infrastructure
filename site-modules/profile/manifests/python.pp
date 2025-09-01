7999d22872577c94a3f795305f1edbb6c808c926
Romain Tartière <romain@FreeBSD.org>
2025-09-01 13:40:28 -1000

    Do not hardcode python version in 3 different places
    The python module does not provide usable versions, so split it in two
    pieces and combine them the appropriate way when needed.  Will allow to
    bump python version at a single place which is still better than what we
    have today.

--- site-modules/profile/manifests/python.pp
+++ site-modules/profile/manifests/python.pp
@@ -1,7 +1,10 @@
 # @summary Manage Python
-class profile::python {
+class profile::python (
+  Integer[0] $major = 3,
+  Integer[0] $minor = 11,
+) {
   class { 'python':
-    version => '311',
+    version => "${major}${minor}",
     dev     => 'present',
   }
 }
